69089820d6a230133ab73c4a88a84b315383f911
Ashok <ashokpatidardev@gmail.com>
2022-07-24 11:26:17 +0530

    create go server and env configuration

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World"))
+}
 func main() {
 	fmt.Println("Go Mongo Restful API")
+	port := os.Getenv("PORT")
+	fmt.Println("Go server is started on", port)
+	router := mux.NewRouter()
+	router.HandleFunc("/", helloWorldHandler)
+	http.ListenAndServe(":"+port, router)
 }
